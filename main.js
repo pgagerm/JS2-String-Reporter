2f6a64c40d45f3bbfbf92ee152bf16d9bcdaead5
Michael Germany <mike@rickgermany.com>
2015-01-19 17:14:25 -0700

    final draft

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+//JS2 String Reporter
+
 var data = prompt("Enter in any word.");
 
-alert(data + " was entered.");
-alert("There are " + data.length + " characters in " + data);
+alert(data + " was entered.\nThere are " + data.length + " characters in " + 
+	data + ".\n" + "The third character is '" + data.charAt(2) + "'.\n" + 
+	"Lowercase: " + data.toLowerCase() + "\n" + "Uppercase: " + data.toUpperCase() + "\n" +
+	data + " is a wonderful word!\n" + "Subword: " + data.substring(1, 4));
+
+
